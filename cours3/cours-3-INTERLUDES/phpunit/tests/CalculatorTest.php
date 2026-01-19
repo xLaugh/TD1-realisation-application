41af06575298aa0b02d1cbbb92a98b615ab89d11
Riole88 <boullemanitou@gmail.com>
2026-01-19 14:42:10 +0100

    Changement cour 3

--- cours3/cours-3-INTERLUDES/phpunit/tests/CalculatorTest.php
+++ cours3/cours-3-INTERLUDES/phpunit/tests/CalculatorTest.php
@@ -2,8 +2,17 @@
 
 namespace Tests;
 
+use App\Calculator;
 use PHPUnit\Framework\TestCase;
 
 class CalculatorTest extends TestCase
 {
+    public function setUp(): void {
+        $this->calculator = new Calculator();
+    }
+
+    public function testAdd(): void {
+        $res = $this->calculator->add(2,2);
+        self::asserrtEquals(4,$res);
+    }
 }
