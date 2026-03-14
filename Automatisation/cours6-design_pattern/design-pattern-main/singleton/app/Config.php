64ce395a4412b7f200d97240eb04994c5bde539e
Laugh <70723697+xLaugh@users.noreply.github.com>
2026-03-14 11:21:41 +0100

    Singleton

--- Automatisation/cours6-design_pattern/design-pattern-main/singleton/app/Config.php
+++ Automatisation/cours6-design_pattern/design-pattern-main/singleton/app/Config.php
@@ -1,5 +1,28 @@
 <?php
 
-# TODO: Créer une classe Config en Singleton
-
 namespace App;
+
+class Config
+{
+    private static $instance = null;
+
+    private $settings = [];
+
+    private function __construct()
+    {
+        $this->settings = require __DIR__ . '/../config/config.php';
+    }
+
+    public static function getInstance(): self
+    {
+        if (self::$instance === null) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+
+    public function get(string $key)
+    {
+        return $this->settings[$key] ?? null;
+    }
+}
--- Automatisation/cours6-design_pattern/design-pattern-main/singleton/public/index.php
+++ Automatisation/cours6-design_pattern/design-pattern-main/singleton/public/index.php
@@ -1,7 +1,16 @@
 <?php
-require('../vendor/autoload.php');
+require dirname(__DIR__) . '/vendor/autoload.php';
 
-die('ici vous pouvez tester votre singleton !');
-# TODO: Récuperer une instance de Config
-# Afficher une valeur contenu dans config.php
-# Récupérer une seconde instance de Config et vérifié que les deux instances sont identiques
+use App\Config;
+
+$config = Config::getInstance();
+
+echo "<pre>\n";
+echo "=== Singleton Config ===\n\n";
+echo "Valeur 'debug' : " . ($config->get('debug') ? 'true' : 'false') . "\n";
+echo "Valeur 'apiKey' : " . $config->get('apiKey') . "\n";
+echo "Valeur 'db' : " . json_encode($config->get('db'), JSON_PRETTY_PRINT) . "\n\n";
+
+$config2 = Config::getInstance();
+echo "Même instance (\$config === \$config2) : " . ($config === $config2 ? 'oui' : 'non') . "\n";
+echo "</pre>";
