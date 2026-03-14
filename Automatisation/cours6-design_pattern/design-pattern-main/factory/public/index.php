7976665fffe547283121f73ce1da0eadb06a4301
Laugh <70723697+xLaugh@users.noreply.github.com>
2026-03-14 10:24:55 +0100

    seance6

--- Automatisation/cours6-design_pattern/design-pattern-main/factory/public/index.php
+++ Automatisation/cours6-design_pattern/design-pattern-main/factory/public/index.php
@@ -1,4 +1,7 @@
 <?php
-require('../vendor/autoload.php');
-die('ici, vous pouvez tester votre factory !');
-# Essayez d'utiliser votre factory ici
+require dirname(__DIR__) . '/vendor/autoload.php';
+
+$vehicle = new \App\Entity\Car(100, 'gasoline');
+
+echo "Coût au km : " . $vehicle->getCostPerKm() . "\n";
+echo "Type de carburant : " . $vehicle->getFuelType() . "\n";
