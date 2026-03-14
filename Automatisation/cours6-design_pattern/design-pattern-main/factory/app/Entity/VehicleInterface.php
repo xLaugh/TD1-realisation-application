bfd8863d004f3e59a133228cb95265912b794d1e
Laugh <70723697+xLaugh@users.noreply.github.com>
2026-03-14 10:36:56 +0100

    Update VehicleInterface.php

--- Automatisation/cours6-design_pattern/design-pattern-main/factory/app/Entity/VehicleInterface.php
+++ Automatisation/cours6-design_pattern/design-pattern-main/factory/app/Entity/VehicleInterface.php
@@ -2,9 +2,17 @@
 
 namespace App\Entity;
 
-interface VehicleInterface
-{
-    public function getCostPerKm();
-
-    public function getFuelType();
+class VehicleFactory {
+    public static function createVehicle($type, $costPerKm, $fuelType) {
+        switch ($type) {
+            case 'car':
+                return new Car($costPerKm, $fuelType);
+            case 'bicycle':
+                return new Bicycle($costPerKm, $fuelType);
+            case 'truck':
+                return new Truck($costPerKm, $fuelType);
+            default:
+                throw new \InvalidArgumentException("Type de véhicule inconnu : $type");
+        }
+    }
 }
